0bf40300686c838a545badf9ee35d4f1f2cd8971
hsnliqdrv <heseneli536@gmail.com>
2025-11-26 22:04:07 +0400

    fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,12 @@
 Coroutines and concurrency
 """
 import asyncio
+import typing
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Spawn wait_random n times and return delays in ascending order"""
+    wait_random = __import__('0-basic_async_syntax').wait_random
     coros = [wait_random(max_delay) for _ in range(n)]
     results = []
     for coro in asyncio.as_completed(coros):
