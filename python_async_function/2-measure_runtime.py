7a851502ec72083e30a4cb4e870db34429c11a8b
hsnliqdrv <heseneli536@gmail.com>
2025-11-26 22:15:54 +0400

    fix

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""2-measure_runtime
+
+Measures total runtime
+"""
+import asyncio
+import time
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures runtime of wait_n"""
+    wait_n = __import__('1-concurrent_coroutines').wait_n
+    start = time.time()
+    await wait_n(n, max_delay)
+    end = time.time()
+    return (end - start) / n
