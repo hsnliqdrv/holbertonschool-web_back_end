22237be8883508f03bb921f850b1f879fb84dda9
hsnliqdrv <heseneli536@gmail.com>
2025-12-14 13:46:28 +0400

    fix

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,6 +35,6 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Gets pages from dataset"""
         assert isinstance(page, int) and isinstance(page_size, int)
-        assert(page > 0 and page_size > 0)
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
