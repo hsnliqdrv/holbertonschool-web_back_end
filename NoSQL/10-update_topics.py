d990fdb451a44e39df1eebc0560cd5dd3306b969
hsnliqdrv <heseneli536@gmail.com>
2025-12-21 15:51:09 +0400

    fix

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """10-update_topics.py"""
 
-
 def update_topics(mongo_collection, name, topics):
-    """Updates all topics of a school document based on the name."""
-    mongo_collection.update_one(
-        {"name": name},     # Filter: find document by name
-        {"$set": {"topics": topics}}  # Update: set the topics field
+    """
+    Updates the topics of a school document based on the name,
+    only if it does not already have topics.
+    """
+    mongo_collection.update_many(
+        {"name": name, "topics": {"$exists": False}},  # filter
+        {"$set": {"topics": topics}}                  # update
     )
+
