a3a5c5f5af10134298353cfaa25e727a464fe515
hsnliqdrv <heseneli536@gmail.com>
2026-01-04 14:18:19 +0400

    fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,5 @@
 export default class Car {
     constructor(brand, motor, color) {
-        if (typeof brand !== 'string') {
-            throw new Error('Brand must be a string');
-        }
-        if (typeof motor !== 'string') {
-            throw new Error('Motor must be a string');
-        }
-        if (typeof color !== 'string') {
-            throw new Error('Color must be a string');
-        }
         this._brand = brand;
         this._motor = motor;
         this._color = color;
@@ -27,6 +18,6 @@ export default class Car {
     }
 
     cloneCar() {
-        return new Car(this._brand, this._motor, this._color);
+        return new this.constructor();
     }
 }
