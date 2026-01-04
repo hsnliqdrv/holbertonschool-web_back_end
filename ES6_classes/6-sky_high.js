3b81ffe1788fe09d593e6eb62252cfb6e7486032
hsnliqdrv <heseneli536@gmail.com>
2026-01-04 12:45:50 +0400

    task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly and carefully from the SkyHighBuilding.';
+  }
+}
