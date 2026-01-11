0bc558594ad176715e350f081aa0a4254bed64fe
hsnliqdrv <heseneli536@gmail.com>
2026-01-11 11:45:50 +0400

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 function cleanSet(set, startString) {
     const result = [];
     for (const item of set) {
+        if (!item || typeof item !== 'string') {
+            continue;
+        }
         if (item.startsWith(startString)) {
             result.push(item.slice(startString.length));
         }
