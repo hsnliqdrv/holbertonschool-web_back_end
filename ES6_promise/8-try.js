6cafc94653105f3c48704e593a83ca3cc80a79e1
hsnliqdrv <heseneli536@gmail.com>
2026-01-18 14:19:49 +0400

    fix

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numberA, numberB) {
     if (numberB === 0) {
-        return Error('cannot divide by 0');
+        throw new Error('cannot divide by 0');
     }
     return numberA / numberB;
 }
